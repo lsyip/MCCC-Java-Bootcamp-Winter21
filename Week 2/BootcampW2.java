b04839d6c5bda9ff3e6ce5748feffe4b7c13cd56
Lynn Yip <50850731+lsyip@users.noreply.github.com>
2022-01-03 19:42:17 -0500

    Add files via upload

---
+++ Week 2/BootcampW2.java
@@ -0,0 +1,15 @@
+public class BootcampW2 {
+    public static void main(String[] argv) {
+        // let's go play with methods of the String class !
+
+        String maryPoppins = "Supercalifragilisticexpialidoious";
+
+            // So I may use the quote "be the change you want to see in the world."
+        String wiseWords = "Put in your favorite quotation here.";
+
+        //1. How many letters are in the maryPoppins variable ?
+        //2. What is the integer location of the first 'x' letter in maryPoppins?
+        //3. Create a new String that is only the second half of the word
+        //4. BONUS ! break up the quotation wiseWords into an array of Strings 
+    }
+}
